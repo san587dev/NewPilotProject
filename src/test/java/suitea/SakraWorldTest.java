5713e124cfb33b9d83ce4136e017eda1809f0779
san587dev <san587dev@gmail.com>
2021-06-02 19:44:24 -0500

    Updated the project.properties with the locators and data. And updated the reports folder with the new reports

--- src/test/java/suitea/SakraWorldTest.java
+++ src/test/java/suitea/SakraWorldTest.java
@@ -14,16 +14,24 @@ public class SakraWorldTest extends TestBase {
         launchBrowser("Chrome");
         log("Opening the browsers");
         driver.get(prop.getProperty("url"));
-        log("opened the URL");
+        log("Navigated to the given URL");
         waitForPageToLoad();
         driver.findElement(By.linkText(prop.getProperty("doctor_name"))).click();
-        log("clicked on the doctor name");
+        log("Clicked on the provided doctor given name");
         /*driver.findElement(By.xpath("//div[3]/a[text()='Dr. Rani Premkumar']")).click();*/
         waitForPageToLoad();
+        Thread.sleep(10000);
         //driver.findElement(By.xpath(prop.getProperty("Consultation_Button"))).click();
-        driver.findElement(By.xpath("//*[@id=\"hv-vc-appointment-pop-up23\"]/div/div/div[2]/div/p[2]/button[1]")).click();
-        log("User clicked Consulting");
-
+        driver.findElement(By.xpath("//*[@id='hv-vc-appointment-pop-up23']/div/div/div[2]/div/p[2]/button[1]")).click();
+        log("User clicked on Consulting");
+        /*waitForPageToLoad();*/
+        Thread.sleep(10000);
+        driver.findElement(By.id(prop.getProperty("name"))).sendKeys(prop.getProperty("first_name"));
+        log("Entering Name in the name field");
+        driver.findElement(By.id(prop.getProperty("email"))).sendKeys(prop.getProperty("email_id"));
+        log("Entering Email address of the user");
+        driver.findElement(By.id(prop.getProperty("mobile"))).sendKeys(prop.getProperty("phoneNo"));
+        log("Entering the mobile phone no");
     }
 
 
